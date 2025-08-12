6cb2b57ad2a46aa47e7e2138c14f74ab0ab1c706
Kopyae87 <kopyae135385@gmail.com>
2025-08-12 14:16:40 +0630

    EnquiryValidators

---
+++ src/org/ace/accounting/system/motor/validator/MotorPolicyVehicleLinkEnquiryValidator.java
@@ -0,0 +1,22 @@
+package org.ace.accounting.system.motor.validator;
+
+import org.ace.accounting.common.validation.IDataValidator;
+import org.ace.accounting.common.validation.ValidationResult;
+import org.ace.accounting.system.motor.MotorPolicyVehicleLink;
+import org.springframework.stereotype.Service;
+
+@Service(value = "MotorPolicyVehicleValidator")
+public class MotorPolicyVehicleLinkEnquiryValidator implements IDataValidator<MotorPolicyVehicleLink>{
+	@Override
+	public ValidationResult validate(MotorPolicyVehicleLink motorPolicyVehicleLink, boolean transaction) {
+	        ValidationResult result = new ValidationResult();
+	        String formId = "motorEntryForm";
+	        String registraionnumber= motorPolicyVehicleLink.getRegistrationNo();
+	        
+	        String regPattern = "^[A-Z]{3} [0-9][A-Z]/[0-9]{4}$";
+	        if (registraionnumber == null || !registraionnumber.matches(regPattern)) {
+	            result.addErrorMessage(formId + ":registrationNo", "Invalid Registration No. Format must be like: YGN 1A/1111");
+	        }
+	        return result;
+	}
+}
