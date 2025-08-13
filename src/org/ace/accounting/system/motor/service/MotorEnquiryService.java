07d64a140b88bed79655bb7290b3b3f47ddfd4c5
Kopyae87 <kopyae135385@gmail.com>
2025-08-13 16:25:28 +0630

    Update MotorEnquiryService.java

--- src/org/ace/accounting/system/motor/service/MotorEnquiryService.java
+++ src/org/ace/accounting/system/motor/service/MotorEnquiryService.java
@@ -23,47 +23,26 @@ public class MotorEnquiryService implements IMotorEnquiryService {
 	@Transactional(propagation = Propagation.REQUIRED, readOnly = true)
 	public List<MotorEnquiryDTO> search(Date policyStartDateFrom, Date policyStartDateTo, String policyNo,
 			String registrationNo) {
-//		StringBuilder hql = new StringBuilder("SELECT mv FROM MotorPolicyVehicleLink mv JOIN mv.motorPolicy mp WHERE 1=1 ");
-//		Map<String, Object> paramMap = new HashMap<>();
-//
-//		if (policyStartDateFrom != null) {
-//			hql.append(" AND mp.policyStartDate >= :startDateFrom");
-//			paramMap.put("startDateFrom", policyStartDateFrom);
-//		}
-//
-//		if (policyStartDateTo != null) {
-//			hql.append(" AND mp.policyStartDate <= :startDateTo");
-//			paramMap.put("startDateTo", policyStartDateTo);
-//		}
-//
-//		if (policyNo != null && !policyNo.isEmpty()) {
-//			hql.append(" AND mp.policyNo = :policyNo");
-//			paramMap.put("policyNo", policyNo);
-//		}
-//
-//		if (registrationNo != null && !registrationNo.isEmpty()) {
-//			hql.append(" AND mv.registrationNo = :registrationNo");
-//			paramMap.put("registrationNo", registrationNo);
-//		}
-//		System.out.println("query:" + hql.toString());
-//		return motorPolicyEnquiryDAO.search(hql.toString(), paramMap);
-//	}
 		
 		Map<String, Object> paramMap = new HashMap<>();
-		StringBuffer hql = new StringBuffer("SELECT new org.ace.accounting.system.motor.MotorEnquiryDTO(");
-		hql.append("mp.policyNo, ")
-		   .append("mv.registrationNo, ")
-		   .append("mp.saleChannel, ")
-		   .append("mp.customerName, ")
-		   .append("mp.branch, ")
-		   .append("mv.claimCount, ")
-		   .append("mv.sumInsured, ")
-		   .append("mv.basicTermPremium, ")
-		   .append("mp.submittedDate) ")
-		   .append("FROM MotorPolicyVehicleLink mv ")
-		   .append("JOIN mv.motorPolicy mp ")
-		   .append("WHERE 1=1 ");
-
+//		StringBuffer hql = new StringBuffer("SELECT new org.ace.accounting.system.motor.MotorEnquiryDTO(");
+//		hql.append("mp.policyNo, ")
+//		   .append("mv.registrationNo, ")
+//		   .append("mp.saleChannel, ")
+//		   .append("mp.customerName, ")
+//		   .append("mp.branch, ")
+//		   .append("mv.claimCount, ")
+//		   .append("mv.sumInsured, ")
+//		   .append("mv.basicTermPremium, ")
+//		   .append("mp.submittedDate) ")
+//		   .append("FROM MotorPolicyVehicleLink mv ")
+//		   .append("JOIN mv.motorPolicy mp ")
+//		   .append("WHERE 1=1 ");
+		StringBuffer hql = new StringBuffer("SELECT new org.ace.accounting.system.motor.MotorEnquiryDTO(mp.policyNo, mv.registrationNo, mp.saleChannel, ");
+        hql.append("mp.customerName, mp.branch, mv.claimCount, mv.sumInsured, ");
+        hql.append("mv.basicTermPremium, mp.submittedDate)");
+        hql.append("FROM MotorPolicy mp JOIN mp.vehicleLinks mv WHERE 1=1");
+        
         if (policyStartDateFrom != null) {
             hql.append(" AND mp.policyStartDate >= :policyStartDateFrom");
             paramMap.put("policyStartDateFrom", policyStartDateFrom);
